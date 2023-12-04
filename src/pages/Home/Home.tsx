605400c9a6f245f2612968bc54688daf417c7cbe
phongtintruong <trung.nt204857@sis.hust.edu.vn>
2023-12-04 22:30:47 +0700

    Add simple home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Home.module.scss';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Navbar from '../../components/Navbar/Navbar';
+import Widget from '../../components/Widget/Widget';
+import Featured from '../../components/Featured/Featured';
+import Chart from '../../components/Chart/Chart';
+import Table from '../../components/Table/ListHotelRoom';
+
+const Home = () => {
+  return (
+    <div className={styles['home']}>
+      <Sidebar />
+      <div className={styles['homeContainer']}>
+        <Navbar />
+        <div className={styles['widgets']}>
+          <Widget type="user" />
+          <Widget type="hotel" />
+          <Widget type="room" />
+          <Widget type="form" />
+        </div>
+        <div className={styles['charts']}>
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className={styles['listContainer']}>
+          <div className={styles['listTitle']}>Latest Transactions</div>
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
